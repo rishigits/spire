9d8f17e32c1338831f649fb61115e35b9a7649b4
rishigits <rishigits@users.noreply.github.com>
2025-08-21 17:12:19 -0400

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -4,5 +4,5 @@ read -ra arr <<< "$@"
 version=${arr[1]}
 trap 0 1 2 ERR
 bash /tmp/linux-on-ibm-z-scripts/Spire/${version}/build_spire.sh -y
-tar cvfz spire-{version}-linux-s390x.tar.gz spire/bin/spire-agent spire/bin/spire-server spire/conf/agent/agent.conf spire/conf/server/server.conf
+tar cvfz spire-${version}-linux-s390x.tar.gz spire/bin/spire-agent spire/bin/spire-server spire/conf/agent/agent.conf spire/conf/server/server.conf
 exit 0
