2cb6c26d3aaf269f8239ea1a807925400eee6abb
rishigits <rishigits@users.noreply.github.com>
2025-08-21 15:41:31 -0400

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -o pipefail
 version=$2
-echo $version
+echo "rishi: var1: {$1} - ${version}"
 trap 0 1 2 ERR
 bash /tmp/linux-on-ibm-z-scripts/Spire/${version}/build_spire.sh -y
 tar cvfz spire-{version}-linux-s390x.tar.gz spire/bin/spire-agent spire/bin/spire-server spire/conf/agent/agent.conf spire/conf/server/server.conf
